83f7a639348df80b3ec67ca1302a246ec80d679e
Andriiko <lesiv433@gmail.com>
2024-10-05 18:28:37 +0300

    login page

--- client/src/core/components/login/login.component.ts
+++ client/src/core/components/login/login.component.ts
@@ -1,12 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
+  form = new FormGroup({
+    username: new FormControl(''),
+    password : new FormControl('')
+  })
 
+  router = inject(Router);
+  authService = inject(AuthService);
+
+  login() {
+    const data = {
+      username: this.form.controls.username.value,
+      password: this.form.controls.password.value,
+    }
+
+    this.authService.login(data).subscribe({
+      next: (response) => {
+        localStorage.setItem('token', response);
+        this.router.navigate(['albums']);
+      },
+      error: (error) => {
+        try {
+          const parsedResponse = JSON.parse(error.error);
+          if (parsedResponse.status === 400) {
+            this.form.setErrors({ invalid: true });
+          }
+        } catch {
+          console.error(error);
+        }
+      }
+    });
+  }
 }
