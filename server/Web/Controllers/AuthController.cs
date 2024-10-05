910de6449bfa51ba0ed7af891a9f8fd57d1f6be6
Andriiko <lesiv433@gmail.com>
2024-10-05 14:43:17 +0300

    auth endpoints

--- server/Web/Controllers/AuthController.cs
+++ server/Web/Controllers/AuthController.cs
@@ -1,4 +1,6 @@
-﻿using Core.Requests;
+﻿using Core.Contracts;
+using Core.Exceptions;
+using Core.Requests;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -6,17 +8,33 @@ namespace Web.Controllers;
 
 [Route("api/[controller]")]
 [ApiController]
-public class AuthController : ControllerBase
+public class AuthController(IAuthService _authService) : ControllerBase
 {
     [HttpPost("register")]
     public async Task<IActionResult> Register(RegisterRequest request)
     {
-        return Ok();
+        try
+        {
+            await _authService.Register(request);
+            return Ok();
+        }
+        catch (UsernameTakenException)
+        {
+            return Conflict();
+        }
     }
 
     [HttpPost("login")]
     public async Task<IActionResult> Login(LoginRequest request)
     {
-        return Ok();
+        try
+        {
+            var response = await _authService.Login(request);
+            return Ok(response);
+        }
+        catch (Exception ex) when (ex is InvalidUsernameException or WrongPasswordException)
+        {
+            return BadRequest();
+        }
     }
 }
